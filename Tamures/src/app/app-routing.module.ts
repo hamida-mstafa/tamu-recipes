de25fd99658edae6fe188a89e2b3f206e03526ea
mmbukane-shy6 <sheilaegeidza@gmail.com>
2020-06-22 17:29:36 +0300

    tamu recepies

--- Tamures/src/app/app-routing.module.ts
+++ Tamures/src/app/app-routing.module.ts
@@ -1,11 +1,27 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ViewComponent } from './components/view/view.component';
+import { ProfileComponent } from './components/profile/profile.component';
+import { UpdateComponent } from './components/update/update.component';
+// import { UpdateComponent } from './components/update/update.component';
 
 
-const routes: Routes = [];
 
-@NgModule({
+const routes: Routes = [
+  {path:'update',component:UpdateComponent},
+  {path:'view',component:ViewComponent},
+  {path:'profile',component:ProfileComponent},
+
+  ];
+
+  @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
-})
-export class AppRoutingModule { }
+  })
+  export class AppRoutingModule { }
+  export const routingComponents=[
+  ViewComponent,
+  ProfileComponent,
+  UpdateComponent,
+
+  ]
--- Tamures/src/app/app.module.ts
+++ Tamures/src/app/app.module.ts
@@ -1,15 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
+import { AppRoutingModule,routingComponents } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ProfileComponent } from './components/profile/profile.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { ViewComponent } from './components/view/view.component';
+import { UpdateComponent } from './components/update/update.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     ProfileComponent,
+    NavbarComponent,
+    ViewComponent,
+    UpdateComponent,
+    routingComponents
 
   ],
   imports: [
