cb085134621a35d9030167b0f8a01e2f02e6cfdb
BA <engineervinceblair@gmail.com>
2020-06-24 02:34:06 +0300

    created the landing page

--- Tamures/src/app/app.module.ts
+++ Tamures/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { HomepageComponent } from './components/homepage/homepage.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomepageComponent
   ],
   imports: [
     BrowserModule,
